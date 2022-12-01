26de946d7142c68581742657fc4583c2d79f7d47
Alain <ark2784@gmail.com>
2022-12-01 10:11:26 -0300

    first commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,63 @@
+import Link from "next/link";
+import Image from "next/image";
+import React, { useState } from "react";
+import Logo from "./Logo";
+import NavItem from "./NavItem";
+import logo from '../public/images/logo.jpg'
+
+const MENU_LIST = [
+  { text: "Inicio", href: "/" },
+  { text: "Acerca de nosotros", href: "/about" },
+  { text: "Contacto", href: "/contact" },
+];
+const Navbar = () => {
+  const [navActive, setNavActive] = useState(null);
+  const [activeIdx, setActiveIdx] = useState(-1);
+
+  return (
+    <header>
+      <nav className={`nav`}>
+        <Link href={"/"}>
+
+        <div className="image"></div>
+        <Image
+        src={logo}
+        alt="Picture of the author"
+        width={90} 
+        height={90}
+        
+        // blurDataURL="data:..." automatically provided
+        // placeholder="blur" // Optional blur-up while loading
+      />
+            
+          
+        </Link>
+        <h1 className="logo">Orma Logitic</h1>
+        
+        <div
+          onClick={() => setNavActive(!navActive)}
+          className={`nav__menu-bar`}
+        >
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className={`${navActive ? "active" : ""} nav__menu-list`}>
+          {MENU_LIST.map((menu, idx) => (
+            <div
+              onClick={() => {
+                setActiveIdx(idx);
+                setNavActive(false);
+              }}
+              key={menu.text}
+            >
+              <NavItem active={activeIdx === idx} {...menu} />
+            </div>
+          ))}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
